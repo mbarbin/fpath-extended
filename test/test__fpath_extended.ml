e3d848294adf455fbcb0647e5a750ebb05468be2
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-02-10 09:07:39 +0100

    Add tests

--- test/test__fpath_extended.ml
+++ test/test__fpath_extended.ml
@@ -19,6 +19,56 @@ end = struct
   [@@deriving compare, hash, sexp_of]
 end
 
+let%expect_test "compare" =
+  let test a b =
+    print_s
+      [%sexp
+        (a : Fpath_pair.t)
+        , (Fpath_pair.compare a b |> Ordering.of_int : Ordering.t)
+        , (b : Fpath_pair.t)]
+  in
+  test
+    { a = Fpath.v "file-a"; b = Fpath.v "file-b" }
+    { a = Fpath.v "file-a"; b = Fpath.v "file-b" };
+  [%expect
+    {|
+    (((a file-a)
+      (b file-b))
+     Equal
+     ((a file-a)
+      (b file-b))) |}];
+  let t = { Fpath_pair.a = Fpath.v "file-a"; b = Fpath.v "file-b" } in
+  test t t;
+  [%expect
+    {|
+    (((a file-a)
+      (b file-b))
+     Equal
+     ((a file-a)
+      (b file-b))) |}];
+  test
+    { a = Fpath.v "file-a"; b = Fpath.v "file-a" }
+    { a = Fpath.v "file-a"; b = Fpath.v "file-b" };
+  [%expect
+    {|
+    (((a file-a)
+      (b file-a))
+     Less
+     ((a file-a)
+      (b file-b))) |}];
+  test
+    { a = Fpath.v "file-b"; b = Fpath.v "file-a" }
+    { a = Fpath.v "file-a"; b = Fpath.v "file-b" };
+  [%expect
+    {|
+    (((a file-b)
+      (b file-a))
+     Greater
+     ((a file-a)
+      (b file-b))) |}];
+  ()
+;;
+
 let%expect_test "hashtbl2" =
   let t = Hashtbl.create (module Fpath_pair) in
   Hashtbl.set t ~key:{ Fpath_pair.a = Fpath.v "file-a"; b = Fpath.v "file-b" } ~data:42;
